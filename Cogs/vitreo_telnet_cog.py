0b0d6b25cf2965bd338de415655d63fdc318c287
Rafael Oliveira <rafael.oliveira@vitreo.com.br>
2021-07-29 17:43:37 -0300

    atvação limpar cache

--- Cogs/vitreo_telnet_cog.py
+++ Cogs/vitreo_telnet_cog.py
@@ -7,15 +7,15 @@ class vitreo_telnet(commands.Cog):
         self.bot = bot
         self.service = vitreo_telnet_service(self.bot)
 
-    # @commands.command(pass_context=True, aliases=['limpar-cache', 'lc', 'LC'])
-    # async def limpar_cache(self, ctx):
-    #     mensagem = await ctx.send('limpando...')
-    #     sucesso = await self.service.limpar_cache()
+    @commands.command(pass_context=True, aliases=['limpar-cache', 'lc', 'LC'])
+    async def limpar_cache(self, ctx):
+        mensagem = await ctx.send('limpando...')
+        sucesso = await self.service.limpar_cache()
 
-    #     if (sucesso):
-    #         await mensagem.edit(content = 'Cache homol foi limpo com sucesso!')
-    #     else:
-    #         await mensagem.edit(content = 'Não consegui limpar o cache, alguma coisa deu errado ):')
+        if (sucesso):
+            await mensagem.edit(content = 'Cache homol foi limpo com sucesso!')
+        else:
+            await mensagem.edit(content = 'Não consegui limpar o cache, alguma coisa deu errado ):')
 
 def setup(bot):
     bot.add_cog(vitreo_telnet(bot))
