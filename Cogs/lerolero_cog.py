0b7f11003feb3a526437d3236cf60082c36b435c
Rafael Oliveira <rafael.oliveira@vitreo.com.br>
2021-10-11 08:25:56 -0300

    Finalização lero lero

--- Cogs/lerolero_cog.py
+++ Cogs/lerolero_cog.py
@@ -1,4 +1,3 @@
-import time
 import discord
 from discord.ext import commands
 import pyttsx3
@@ -13,60 +12,12 @@ class lerolero_cog(commands.Cog):
     @commands.command(pass_context=True, aliases=['lero'])
     async def frase_aleatoria(self, ctx):
         frase_aleatoria = await self.service.obter_nova_frase();
-
-        canal = ctx.message.author.voice.channel
-
-        if not canal:
-            await ctx.send("Você não está conectado a um canal de voz.")
-            return
-        
-        channel = ctx.author.voice.channel
-        await channel.connect()
-
-        try:
-            engine = pyttsx3.init()
-            engine.setProperty('rate',180)
-            engine.save_to_file(frase_aleatoria, 'speech.mp3')
-            engine.runAndWait()
-
-            audio_source = discord.FFmpegPCMAudio('speech.mp3')
-
-            await ctx.voice_client.play(audio_source, after=lambda e: print('Player error: %s' % e) if e else None)
-            
-        except Exception as e:
-            print(e)
-
         await ctx.send(frase_aleatoria)
-        await ctx.voice_client.disconnect()
 
     @commands.command(pass_context=True)
     async def dilma(self, ctx):
         frase_aleatoria = await self.service.obter_frase_dilma()
-
-        canal = ctx.message.author.voice.channel
-
-        if not canal:
-            await ctx.send("Você não está conectado a um canal de voz.")
-            return
-        
-        channel = ctx.author.voice.channel
-        await channel.connect()
-
-        try:
-            engine = pyttsx3.init()
-            engine.setProperty('rate',180)
-            engine.save_to_file(frase_aleatoria, 'speech.mp3')
-            engine.runAndWait()
-
-            audio_source = discord.FFmpegPCMAudio('speech.mp3')
-
-            await ctx.voice_client.play(audio_source, after=lambda e: print('Player error: %s' % e) if e else None)
-            
-        except Exception as e:
-            print(e)
-
         await ctx.send(frase_aleatoria)
-        #await ctx.voice_client.disconnect()
 
 
     async def enviar_audio(self, ctx, frase_aleatoria):
